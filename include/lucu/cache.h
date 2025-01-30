38a3d808a64aaa8b44ef9b876c8739bc83f5422b
Lucy Robillard <larobitrumpet@lucyrobillard.xyz>
2025-01-29 19:15:26 -0800

    Add public LucuCache functions to header

--- include/lucu/cache.h
+++ include/lucu/cache.h
@@ -6,4 +6,10 @@
 typedef struct LucuCacheData LucuCacheData;
 typedef LucuCacheData* LucuCache;
 
+LucuCache lucu_cache_new(const int cache_size, const size_t key_size, const size_t value_size, bool (*keys_equal_function)(void*, void*, void*), void* keys_equal_function_params, void* (*generate_function)(void*), void (*key_free_function)(void*), void (*value_free_function)(void*));
+
+void lucu_cache_destroy(LucuCache cache);
+
+void* lucu_cache_get(LucuCache cache, void* key);
+
 #endif
